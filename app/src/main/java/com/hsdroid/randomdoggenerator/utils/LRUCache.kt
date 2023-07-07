7059a607789defd33a6dac8d5d71059995cac7a1
hsinha76 <hsinha76@gmail.com>
2023-07-07 12:52:27 +0530

    Resolved issue with LRUCache malfunctioning

--- app/src/main/java/com/hsdroid/randomdoggenerator/utils/LRUCache.kt
+++ app/src/main/java/com/hsdroid/randomdoggenerator/utils/LRUCache.kt
@@ -1,37 +1,46 @@
 package com.hsdroid.randomdoggenerator.utils
 
 import android.content.Context
+import android.util.Log
 
 class LRUCache(
     val context: Context,
     private val size: Int,
-    private val sharedPreferenceUtil : SharedPreferenceUtil
+    private val sharedPreferenceUtil: SharedPreferenceUtil
 ) {
 
     val lruCache: MutableList<String> = mutableListOf()
 
     fun onRestart() {
-        if (lruCache.size >= size) {
-            lruCache.removeAt(0)
-        }
 
         if (sharedPreferenceUtil.getStringPreference(Constants.CACHE_LIST, null) != null) {
             val replacement = sharedPreferenceUtil.getStringPreference(Constants.CACHE_LIST, null)
                 .replace("[", "")
                 .replace("]", "")
-            lruCache.add(replacement)
+
+            lruCache.addAll(replacement.split(", "))
         }
+
+        Log.d("harish", "on start $lruCache")
     }
 
     fun putData(url: String) {
-        if (lruCache.size >= size) {
-            lruCache.removeAt(0)
-        }
+
+        val itemsString = sharedPreferenceUtil.getStringPreference(Constants.CACHE_LIST, null)
+
+        val itemCount = itemsString?.trim('[', ']') // Remove leading and trailing brackets
+            ?.split(", ") // Split the string into individual items
+            ?.map { it.trim() } // Remove any leading or trailing whitespace
+            ?.size ?: 0
 
         lruCache.add(url)
 
-        sharedPreferenceUtil.setStringPreference(Constants.CACHE_LIST, lruCache.toString())
+        if (itemCount >= size) {
+            Log.d("harish", "element at 0 " + lruCache.elementAt(0))
+            lruCache.removeAt(0)
+        }
 
+        sharedPreferenceUtil.setStringPreference(Constants.CACHE_LIST, lruCache.toString())
     }
 
     fun displayData(): List<String> {
